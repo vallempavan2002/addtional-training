af777e233ee22b96e4f9911dd8e751816722df1e
Pavan kalyan Vallem <116752882+vallempavan2002@users.noreply.github.com>
2023-03-28 22:35:37 +0530

    Update SalesAggregator.java

--- task1/SalesAggregator.java
+++ task1/SalesAggregator.java
@@ -1,32 +1,45 @@
-import java.util.*;
-//import javafx.scene.chart.PieChart.Data;
+import java.util.ArrayList;
+import java.util.List;
 
- public class SalesAggregator {
-    public static void main(String[] args) throws Exception{
-        List<Data> sales22=new ArrayList<>();
-        sales22.add(new Data("Tea",120));
-        sales22.add(new Data("Cofee",230));
-        sales22.add(new Data("Green Tea",60));
-        sales22.add(new Data("Orange Soda",210));
-        sales22.add(new Data("Lemon Soda",205));
+public class SalesAnalysis {
+    private static final String[] PRODUCTS = {"Tea", "Coffee", "GreenTea", "LemonTea", "Soda", "Limca"};
+    private static final double[] SALES_2021 = {100.0, 129.0, 140.0, 167.0, 267.0, 873.0};
+    private static final double[] SALES_2022 = {120.0, 234.0, 168.0, 345.0, 987.0, 983.0};
 
-
-        
-        List<Data> sales21=new ArrayList<>();
-        sales21.add(new Data("Tea",100));
-        sales21.add(new Data("Cofee",250));
-        sales21.add(new Data("Green Tea",50));
-        sales21.add(new Data("Orange Soda",100));
-        sales21.add(new Data("Lemon Soda",220));
-
-
-        //Logic to calculate Variance
-        for(int i=0;i<sales22.size();i++){
-            System.out.println(sales22.get(i));
+    public static void main(String[] args) {
+        List<Product> products = new ArrayList<>();
+        for (int i = 0; i < PRODUCTS.length; i++) {
+            products.add(new Product(PRODUCTS[i], SALES_2021[i], SALES_2022[i]));
         }
 
-        
+        System.out.println("Product\t\tSales2021\tSales2022\tVariance\tVariancePct\tTotal");
+        for (Product p : products) {
+            System.out.printf("%s\t\t%.2f\t\t%.2f\t\t%.2f\t\t%.2f%%\t\t%.2f\n",
+                    p.name, p.sales2021, p.sales2022, p.variance(), p.variancePct(), p.total());
+        }
     }
 
+    static class Product {
+        private final String name;
+        private final double sales2021;
+        private final double sales2022;
 
- }
+        Product(String name, double sales2021, double sales2022) {
+            this.name = name;
+            this.sales2021 = sales2021;
+            this.sales2022 = sales2022;
+        }
+
+        double variance() {
+            return sales2022 - sales2021;
+        }
+
+        double variancePct() {
+            return (sales2022 - sales2021) / sales2021 * 100;
+        }
+
+        double total() {
+            return sales2021 + sales2022;
+        }
+    }
+}
